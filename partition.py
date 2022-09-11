8ad70cce3fb7ce23282218573ca8d5042ec4a8d6
Juhi Chandalia <jkchandalia@gmail.com>
2022-09-10 23:13:01 -0400

    Two pointer algo work

--- partition.py
+++ partition.py
@@ -7,4 +7,25 @@ def partition(arr, start, end, pivot_index):
             arr[pointer_division], arr[i] = arr[i], arr[pointer_division]
             pointer_division+=1
     arr[end], arr[pointer_division] = arr[pointer_division], arr[end]
+    return pointer_division
         
+
+def partition2(arr, start, end, pivot_index):
+    #This does not work yet
+    pointer_less = start
+    arr[pivot_index], arr[end] = arr[end], arr[pivot_index]
+    pointer_more = end - 1
+    print(arr[end])
+    i = pointer_less
+    while pointer_less <= pointer_more:
+        if arr[i] <= arr[end]:
+            arr[pointer_less], arr[i] = arr[i], arr[pointer_less]
+            pointer_less+=1
+            i+=1
+        elif arr[i] > arr[end]:
+            arr[pointer_more], arr[i] = arr[i], arr[pointer_more]
+            pointer_more-=1
+    arr[pointer_more], arr[end] = arr[end], arr[pointer_more]
+    return pointer_more
+arr2 =  [2, 5, -3, 20, 16, 1, 11, -10, 30, 50, 3, 4]
+partition2(arr2, 0, 11, 10)
